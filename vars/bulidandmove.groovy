d07d81e2bec1d1c5623c3494d69c3059e3d2e50a
dushyantraj <125907422+dushyantraj@users.noreply.github.com>
2023-03-02 11:54:55 +0530

    Update bulidandmove.groovy

--- vars/bulidandmove.groovy
+++ vars/bulidandmove.groovy
@@ -1,6 +1,5 @@
-def call(String ip)
+def call()
 {
    sh ' echo "Hello world" > /var/www/html/index.html'
-   sh "scp -i ~/.ssh/id_rsa /var/www/html/index.html  ubuntu@${ip}:/var/www/html/index.html"
 }
 
