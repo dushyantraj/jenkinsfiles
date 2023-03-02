70dbaff5ff4aa27320959742cdb236e9fa6497cc
dushyantraj <125907422+dushyantraj@users.noreply.github.com>
2023-03-02 10:52:22 +0530

    Update bulidfor.groovy

--- vars/bulidfor.groovy
+++ vars/bulidfor.groovy
@@ -1,5 +1,5 @@
-def call()
+def call(String ip)
 {
    sh ' echo "Hello world" > /var/www/html/index.html'
-   sh 'scp -i ~/.ssh/id_rsa /var/www/html/index.html  ubuntu@13.233.123.183:/var/www/html/index.html'
+   sh 'scp -i ~/.ssh/id_rsa /var/www/html/index.html  ubuntu@ip:/var/www/html/index.html'
 }
