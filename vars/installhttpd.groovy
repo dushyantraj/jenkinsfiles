b7067349b0c4f2f7247a305cd7e1883fc27eb692
dushyantraj <125907422+dushyantraj@users.noreply.github.com>
2023-03-02 09:06:33 +0530

    Update installhttpd.groovy

--- vars/installhttpd.groovy
+++ vars/installhttpd.groovy
@@ -1,4 +1,6 @@
 def call() {
-    sh 'sudo apt-get update'
+    sh 'sudo apt-get update -y'
     sh 'sudo apt-get -y install apache2'
+    sh 'sudo  systemctl start apache2'
+    sh 'sudo  systemctl enable apache2'
 }
